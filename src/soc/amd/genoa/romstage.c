922bb6d0f7daea274f5fac7db3e40c3500614b66
Arthur Heymans <arthur.heymans@9elements.com>
2022-07-13 15:59:15 +0200

    soc/amd/genoa: Use TOP_MEM msr to initialise cbmem
    This does not account for TSEG or other UMA but is good enough for now.
    
    Change-Id: I3ca89d32ac80521cbef71a14e3cbbc19c3462ffb
    Signed-off-by: Arthur Heymans <arthur.heymans@9elements.com>

--- src/soc/amd/genoa/romstage.c
+++ src/soc/amd/genoa/romstage.c
@@ -1,9 +1,31 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
+#include <arch/cpu.h>
+#include <arch/stages.h>
+#include <cbmem.h>
 #include <console/console.h>
+#include <program_loading.h>
 #include <romstage_common.h>
+#include <stdint.h>
+#include <timestamp.h>
+#include <cpu/amd/mtrr.h>
+
+void *cbmem_top_chipset(void)
+{
+	/* This SOC has no graphic device so we can just use TOP_MEM */
+	uintptr_t top_mem = rdmsr(TOP_MEM).lo;
+	top_mem -= CONFIG_SMM_TSEG_SIZE;
+	/*
+	 * This MSR has an 8M granularity.
+	 * TSEG also needs to be aligned to its size so account for potentially ill aligned TOP_MEM.
+	 */
+	return (void *)ALIGN_DOWN(top_mem, MAX(8 * MiB, CONFIG_SMM_TSEG_SIZE));
+}
 
 void __noreturn romstage_main(void)
 {
-	die("Not implemented\n");
+	post_code(0x40);
+
+	cbmem_initialize_empty();
+	run_ramstage();
 }
