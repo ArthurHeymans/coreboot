9aed6c96dda0269efaa3cd6719e8defb698f4dff
Arthur Heymans <arthur.heymans@9elements.com>
2022-08-17 14:50:16 +0200

    vendorcode/amd/opensil: Add Returncode handling
    Print the opensil return code.
    
    Change-Id: I6ab5afa00e7dcb6785389a7ecc7741d24c887f85
    Signed-off-by: Arthur Heymans <arthur.heymans@9elements.com>

--- src/vendorcode/amd/opensil/ramstage.c
+++ src/vendorcode/amd/opensil/ramstage.c
@@ -8,6 +8,39 @@
 #include <string.h>
 #include <xSIM-api.h>
 
+static void SIL_STATUS_report(const char *function, const SIL_STATUS status)
+{
+	const int log_level = status == SilPass ? BIOS_DEBUG : BIOS_ERR;
+	const char *error_string = "Unkown error";
+
+	const struct error_string {
+		SIL_STATUS status;
+		const char *string;
+	} errors[] = {
+		{SilPass, "SilPass"},
+		{SilUnsupportedHardware, "SilUnsupportedHardware"},
+		{SilUnsupported, "SilUnsupported"},
+		{SilInvalidParameter, "SilInvalidParameter"},
+		{SilAborted, "SilAborted"},
+		{SilOutOfResources, "SilOutOfResources"},
+		{SilNotFound, "SilNotFound"},
+		{SilOutOfBounds, "SilOutOfBounds"},
+		{SilDeviceError, "SilDeviceError"},
+		{SilResetRequestColdImm, "SilResetRequestColdImm"},
+		{SilResetRequestColdDef, "SilResetRequestColdDef"},
+		{SilResetRequestWarmImm, "SilResetRequestWarmImm"},
+		{SilResetRequestWarmDef, "SilResetRequestWarmDef"},
+	};
+
+	int i;
+	for (i = 0; i < ARRAY_SIZE(errors); i++) {
+		if (errors[i].status == status)
+			error_string = errors[i].string;
+	}
+	printk(log_level, "%s returned %d (%s)\n", function, status, error_string);
+
+}
+
 static void HostDebugService(size_t MsgLevel, const char *SilPrefix, const char *Message,
 				 const char *Function, size_t Line, ...)
 {
@@ -36,13 +69,15 @@ static void setup_opensil(void)
 	if (done)
 		return;
 
-	SilDebugSetup(HostDebugService);
+	const SIL_STATUS debug_ret = SilDebugSetup(HostDebugService);
+	SIL_STATUS_report("SilDebugSetup", debug_ret);
 	const size_t MemReq = xSimQueryMemoryRequirements();
 	void *MemBuf = cbmem_add(CBMEM_ID_AMD_OPENSIL, MemReq);
 	if (!MemBuf)
 		die("%s: failed to alloc mem\n", __func__);
 	/* We run all opensil timepoints in the same stage so using TP1 as argument is fine. */
-	xSimAssignMemory(MemBuf, MemReq, SIL_TP1);
+	const SIL_STATUS assign_mem_ret =xSimAssignMemory(MemBuf, MemReq, SIL_TP1);
+	SIL_STATUS_report("xSimAssignMemory", assign_mem_ret);
 	done = true;
 }
 
@@ -50,7 +85,8 @@ static void tp1_opensil(void *timepoint)
 {
 	setup_opensil();
 
-	InitializeAMDSi();
+	const SIL_STATUS ret = InitializeAMDSi();
+	SIL_STATUS_report("InitializeAMDSi", ret);
 }
 
 BOOT_STATE_INIT_ENTRY(BS_PRE_DEVICE, BS_ON_EXIT, tp1_opensil, SIL_TP1);
