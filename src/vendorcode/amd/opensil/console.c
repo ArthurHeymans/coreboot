481da801efa9ff5876e4018704108211c8af3b41
Arthur Heymans <arthur.heymans@9elements.com>
2023-01-09 10:55:10 +0100

    vendorcode/amd/opensil: Translate console levels
    Translate console loglevels to coreboot ones.
    
    Change-Id: I09199d1fb491d1c85b29895897fd0ae62b312adf
    Signed-off-by: Arthur Heymans <arthur.heymans@9elements.com>

--- src/vendorcode/amd/opensil/console.c
+++ src/vendorcode/amd/opensil/console.c
@@ -5,6 +5,24 @@
 #include <string.h>
 #include <console/console.h>
 #include <vendorcode/amd/opensil/console.h>
+#include <SilCommon.h>
+
+static int translate_opensil_debug_level(size_t MsgLevel)
+{
+	switch (MsgLevel) {
+	case SIL_TRACE_ERROR:
+		return BIOS_ERR;
+	case SIL_TRACE_WARNING:
+		return BIOS_WARNING;
+	case SIL_TRACE_ENTRY:
+	case SIL_TRACE_EXIT:
+		return BIOS_SPEW;
+	case SIL_TRACE_INFO:
+		return BIOS_DEBUG;
+	default:
+		return BIOS_NEVER;
+	}
+}
 
 void HostDebugService(size_t MsgLevel, const char *SilPrefix, const char *Message,
 				 const char *Function, size_t Line, ...)
@@ -23,9 +41,6 @@ void HostDebugService(size_t MsgLevel, const char *SilPrefix, const char *Messag
 	vsnprintf(msg, sizeof(msg), Message, args);
 	va_end(args);
 
-	/*
-	 * Combine prefix & message to a coreboot message
-	 * TODO: translate opensil message levels to coreboot ones.
-	 */
-	printk(BIOS_DEBUG, "L=%lu,%s%s", MsgLevel, prefix, msg);
+	const int loglevel = translate_opensil_debug_level(MsgLevel);
+	printk(loglevel, "%s%s", prefix, msg);
 }
