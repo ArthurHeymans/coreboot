bd31b88308c6eecdb217d54cf01793fa613275ed
Arthur Heymans <arthur.heymans@9elements.com>
2023-04-11 12:38:36 +0200

    soc/amd/genoa: Remove used files
    Change-Id: I45f0e9b9c3fbdfebde2ad6fbe64f2dd3599616b2
    Signed-off-by: Arthur Heymans <arthur.heymans@9elements.com>

--- src/soc/amd/genoa/bootblock.c
+++
@@ -1,9 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0-only */
-
-#include <amdblocks/amd_pci_mmconf.h>
-#include <amdblocks/cpu.h>
-#include <bootblock_common.h>
-#include <console/console.h>
-#include <cpu/x86/tsc.h>
-#include <soc/southbridge.h>
-#include <stdint.h>
