3ec23a2167bddb545c76a0b373eeac144474903e
Arthur Heymans <arthur@aheymans.xyz>
2023-06-15 10:59:49 +0200

    soc/amd/common/iommu.c: Make sure iommu is enabled
    Don't rely on vendorcode to set enable bit on IOMMU.
    
    Signed-off-by: Arthur Heymans <arthur@aheymans.xyz>
    Signed-off-by: Naresh Solanki <Naresh.Solanki@9elements.com>
    Change-Id: I1805a20656b7fb3915f8cc93c618ee074461840f

--- src/soc/amd/common/block/iommu/iommu.c
+++ src/soc/amd/common/block/iommu/iommu.c
@@ -1,5 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
+#include <console/console.h>
 #include <device/device.h>
 #include <device/pci.h>
 #include <lib.h>
@@ -20,6 +21,16 @@ static void iommu_read_resources(struct device *dev)
 	res->flags = IORESOURCE_MEM;
 }
 
+
+static void iommu_enable_resources(struct device *dev)
+{
+	uint32_t base = pci_read_config32(dev, 0x44);
+	base |= 1;
+	pci_write_config32(dev, 0x44, base);
+	printk(BIOS_DEBUG, "%s -> mmio enable: %08X", __func__, pci_read_config32(dev, 0x44));
+	pci_dev_enable_resources(dev);
+}
+
 #if CONFIG(HAVE_ACPI_TABLES)
 static const char *iommu_acpi_name(const struct device *dev)
 {
@@ -30,7 +41,7 @@ static const char *iommu_acpi_name(const struct device *dev)
 struct device_operations amd_iommu_ops = {
 	.read_resources = iommu_read_resources,
 	.set_resources = pci_dev_set_resources,
-	.enable_resources = pci_dev_enable_resources,
+	.enable_resources = iommu_enable_resources,
 	.ops_pci = &pci_dev_ops_pci,
 #if CONFIG(HAVE_ACPI_TABLES)
 	.acpi_name = iommu_acpi_name,
